b61bb3441e1952e66232b77ced446ebe3a3ebde1
tyarini <ellybratatyarinii@gmail.com>
2025-11-12 04:31:45 +0700

    AGEN LITE 0.1

---
+++ story_content/5lkXQtH9GdP_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/5lkXQtH9GdP_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.105.35604.0%0D%0A%0D%0A00:00:00.100%20--%3E%2000:00:01.963%0D%0A[button%20cartoon]%0D%0A%0D%0A"}]})
